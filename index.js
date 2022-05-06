2cc625eb07ae8d7e06ac52978b41246de67ceb66
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-07 03:53:40 +0600

    envupdate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-// MiddleWare
+// Middleware
 app.use(cors());
 app.use(express.json());
 
