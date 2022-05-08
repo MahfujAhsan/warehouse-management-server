59d864acc13bc71dd803e54e5223fb98247cf5d9
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-08 21:35:03 +0600

    jwt updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -9,39 +10,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@molinardcluster.ixezn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
 
 
 async function run() {
     try {
         await client.connect();
         const inventory = client.db('molinardInventory').collection('inventoryIteams');
-        app.get('/inventory', async (req, res) => {
-            const email = req.query.email;
-            const query = {email};
-            const cursor = inventory.find(query);
-            const inventoryItems = await cursor.toArray();
-            res.send(inventoryItems);
-        });
-        app.get('/inventory/:itemId', async(req, res) => {
+
+        app.get('/inventory/:itemId', async (req, res) => {
             const id = req.params.itemId;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const item = await inventory.findOne(query);
             res.send(item);
         });
-        app.post('/inventory', async(req, res) => {
+        app.post('/inventory', async (req, res) => {
             const newItem = req.body;
-            const result = await inventory.insertOne(newItem);
-            res.send(result);
+            const tokenInfo = req.headers.authorization;
+            const [email, accessToken] = tokenInfo.split(" ")
+            const decoded = verifyToken(accessToken);
+            console.log(decoded)
+            if (email === decoded.email) {
+                const result = await inventory.insertOne(newItem);
+                res.send(result);
+            }
+            else{
+                res.send({success: "Unauthorized Access"})
+            }
+
         });
-        app.put('/inventory/:itemId', async(req, res) => {
+
+        app.put('/inventory/:itemId', async (req, res) => {
             const id = req.params.itemId;
             const updatedItem = req.body;
-            const item = {_id: ObjectId(id)};
-            const options = {upsert: true};
+            const item = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const updatedDoc = {
                 $set: {
                     quantity: updatedItem.quantity
@@ -50,11 +54,21 @@ async function run() {
             const result = await inventory.updateOne(item, updatedDoc, options);
             res.send(result);
         });
-        app.delete('/inventory/:itemId', async(req, res) => {
+        app.delete('/inventory/:itemId', async (req, res) => {
             const id = req.params.itemId;
-            const item = {_id: ObjectId(id)};
+            const item = { _id: ObjectId(id) };
             const result = await inventory.deleteOne(item);
             res.send(result);
+        });
+
+        app.delete("/inventory", (req, res) => {
+            items = [];
+            res.redirect("/inventory");
+        });
+        app.post('/login', (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN);
+            res.send({ token });
         });
 
     }
@@ -72,3 +86,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log('Molinard is Boom');
 });
+
+function verifyToken(token) {
+    let email
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if(err) {
+            email = 'Invalid Email'
+        }
+        if (decoded) {
+            email = decoded
+        }
+    });
+    return email;
+}
