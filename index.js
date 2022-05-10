c1b9c42388e906b8cdb84c9f1c3d85bf343ca941
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-11 00:26:22 +0600

    finalize

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // MiddleWare
 app.use(cors());
