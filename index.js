cb03bf0cd9ef73886cc36e996b473c8a85d7d8db
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-11 12:12:23 +0600

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5500;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.46ry8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://MahfujAhsan:dNFUJ0vqxFsVUhme@cluster0.46ry8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
